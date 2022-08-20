a93c4a8b339a57b6b33454ad5c12f00e18dca442
Brian Drelling <brian@drelling.com>
2022-08-19 19:48:34 -0500

    Removed PianoRoll middleware

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,13 +5,9 @@ public func configure(_ app: Application) throws {
     // serve files from /Public folder
      app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    let audioKitURL = URL(fileURLWithPath: "\(app.directory.publicDirectory)/documentation/AudioKit.doccarchive")
+    let audioKitURL = URL(fileURLWithPath: "\(app.directory.publicDirectory)documentation/AudioKit.doccarchive")
     
     app.middleware.use(DocCMiddleware(archivePath: audioKitURL))
-    
-    let pianoRollURL = URL(fileURLWithPath: "\(app.directory.publicDirectory)/documentation/AudioKit.doccarchive")
-    
-    app.middleware.use(DocCMiddleware(archivePath: pianoRollURL))
 
     // register routes
     try routes(app)
