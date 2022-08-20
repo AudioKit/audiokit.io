594b7e680439aadecede873b047d1755abdf8da9
Brian Drelling <brian@drelling.com>
2022-08-20 14:32:41 -0500

    Updated docgen script

--- scripts/docgen.sh
+++ scripts/docgen.sh
@@ -3,15 +3,11 @@
 # Define our working directory.
 working_directory=$(pwd)
 
-# Define our documentation directory.
-# This corresponds to Vapor's public file directory.
-documentation_directory="${working_directory}/Public/documentation"
-
 # Define our products to generate documentation for.
 # products='AudioKit PianoRoll'
 
 # Define our temporary directory for cloning repositories, which ensures proper cleanup.
-temp_directory="${working_directory}/temp"
+temp_directory="${working_directory}/.temp"
 
 # If our command failed previously, we may need to delete the temporary directory.
 rm -rf $temp_directory
@@ -24,13 +20,17 @@ cd $temp_directory
 git clone https://github.com/bdrelling/AudioKitDocs
 cd AudioKitDocs
 
+# Define our documentation directory.
+# This corresponds to Vapor's public file directory.
+documentation_directory="${working_directory}/Public/documentation"
+
 # Delete our documentation directory and recreate it.
 # This ensure stale documentation won't carry-over, such as in the event of an error.
 rm -rf $documentation_directory
 mkdir $documentation_directory
 
 # Run the docgen command.
-swift run docgen AudioKit PianoRoll --output $documentation_directory
+./docgen.sh $documentation_directory
 
 # Finally, remove our temp directory and all cloned repositories.
 rm -rf $temp_directory
