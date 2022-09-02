2405eb9328a34bbbf5b48d8925954abeb881b805
Aurelius Prochazka <aurephd@gmail.com>
2022-09-01 23:26:53 -0700

    Added menu scripts

---
+++ Public/js/insertmenu.js
@@ -0,0 +1,20 @@
+document.write('<div id="sse2">\
+<div id="sses2">\
+    <ul>\
+        <li><a href="/">Home</a></li>\
+        <li><a href="/AudioKit/">AudioKit</a></li>\
+        <li><a href="/AudioKitEX/">AudioKitEX</a></li>\
+        <li><a href="/AudioKitUI/">AudioKitUI</a></li>\
+        <li><a href="/DevoloopAudioKit/">DevoloopAudioKit</a></li>\
+        <li><a href="/DunneAudioKit/">DunneAudioKit</a></li>\
+        <li><a href="/Keyboard">Keyboard</a></li>\
+        <li><a href="/Microtonality">Microtonality</a></li>\
+        <li><a href="/PianoRoll">Piano Roll</a></li>\
+        <li><a href="/SoundpipeAudioKit/">SoundpipeAudioKit</a></li>\
+        <li><a href="/SporthAudioKit/">SporthAudioKit</a></li>\
+        <li><a href="/STKAudioKit/">STKAudioKit</a></li>\
+        <li><a href="/Tonic">Tonic</a></li>\
+    </ul>\
+</div>\
+</div>\
+');
