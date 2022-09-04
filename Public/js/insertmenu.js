78e5fa4a78c96d4496815f891be8556632df127e
Aurelius Prochazka <aurephd@gmail.com>
2022-09-04 12:56:05 -0700

    Simpler menu

--- Public/js/insertmenu.js
+++ Public/js/insertmenu.js
@@ -2,19 +2,12 @@ document.write('<div id="sse2">\
 <div id="sses2">\
     <ul>\
         <li><a href="/">Home</a></li>\
-        <li><a href="/AudioKit/">AudioKit</a></li>\
-        <li><a href="/AudioKitEX/">AKEX</a></li>\
-        <li><a href="/AudioKitUI/">AKUI</a></li>\
-        <li><a href="/DevoloopAudioKit/">Devoloop</a></li>\
-        <li><a href="/DunneAudioKit/">Dunne</a></li>\
-        <li><a href="/NodeEditor">Flow</a></li>\
+        <li><a href="/AudioKit">AudioKit</a></li>\
+        <li><a href="/Flow">Flow</a></li>\
         <li><a href="/Keyboard">Keyboard</a></li>\
-        <li><a href="/Microtonality">Microtonality</a></li>\
         <li><a href="/PianoRoll">Piano Roll</a></li>\
-        <li><a href="/SoundpipeAudioKit/">SP</a></li>\
-        <li><a href="/SporthAudioKit/">Sporth</a></li>\
-        <li><a href="/STKAudioKit/">STK</a></li>\
         <li><a href="/Tonic">Tonic</a></li>\
+        <li><a href="/">...More</a></li>\
     </ul>\
 </div>\
 </div>\
